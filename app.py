369821b69b9244107776d613f87b96c93b4aeb48
Pontom <motnop@gmail.com>
2020-03-10 20:59:09 +0300

    Done 4 Chapter

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField, HiddenField, SubmitField, RadioField
+from wtforms.validators import InputRequired, Email, Regexp
 from helpfunctions import read_json
 from helpfunctions import add_data_to_json
 
@@ -11,6 +15,7 @@ GOALS_DATA = read_json('goals.json')
 WEEKDAYS_NAMES = read_json('weekdays.json')
 
 app = Flask(__name__)
+app.secret_key = 'my-key-what-was'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
@@ -38,9 +43,9 @@ class Booking(db.Model):
     __tablename__ = "bookings"
     clientId = db.Column(db.Integer, primary_key=True)
     clientName = db.Column(db.String(100))
-    clientPhone = db.Column(db.String(20))
+    clientPhone = db.Column(db.String(25))
     clientWeekday = db.Column(db.String(20))
-    clientTime = db.Column(db.Time)
+    clientTime = db.Column(db.String(20))
     clientTeacher_id = db.Column(db.Integer, db.ForeignKey("teachers.id"))
     clientTeacher = db.relationship("Teacher", back_populates="bookings")
 
@@ -52,12 +57,43 @@ class Search(db.Model):
     __tablename__ = "search"
     clientId = db.Column(db.Integer, primary_key=True)
     clientGoal = db.Column(db.String(20))
-    clientTime = db.Column(db.Time)
+    clientTime = db.Column(db.String(25))
     clientName = db.Column(db.String(100))
     clientPhone = db.Column(db.String(20))
 
-
+#инициализация Таблиц
 db.create_all()
+
+
+class BookinForm(FlaskForm):
+    """
+    Форма резервирования времени предподователя
+    """
+    clientWeekday = HiddenField('')
+    clientTime = HiddenField('')
+    clientTeacher = HiddenField('')
+    clientName = StringField('Вас зовут', [InputRequired()])
+    clientPhone = StringField('Ваш телефон', [InputRequired(), Regexp(regex=r'\+7 \d{3} \d{3} \d{4}', message='Телефон не соответсвует формату +7 XXX XXX XXXX')])
+    submit = SubmitField('Записаться на пробный урок')
+
+
+class SearchForm(FlaskForm):
+    """
+    Форма по подбору предподователя
+    """
+    clientTimeRadio = [
+                       ("1-2", "1-2 часа в&nbsp;неделю"),
+                       ("3-5", "3-5 часов в&nbsp;неделю"),
+                       ("5-7", "5-7 часов в&nbsp;неделю"),
+                       ("7-10", "7-10 часов в&nbsp;неделю")
+                       ]
+    clientGoal = RadioField('Какая цель занятий?',
+                            choices=[(name, value['value']) for name, value in GOALS_DATA.items()])
+    clientTime = RadioField('Сколько времени есть?', choices=clientTimeRadio)
+    clientName = StringField('Вас зовут', [InputRequired()])
+    clientPhone = StringField('Ваш телефон', [InputRequired(), Regexp(regex=r'\+7 \d{3} \d{3} \d{4}', message='Телефон не соответсвует формату +7 XXX XXX XXXX')])
+    submit = SubmitField('Найдите мне преподавателя')
+
 
 """
 #### Устаревшая функция по переносу данных их файла data.py
@@ -78,13 +114,14 @@ def import_db_from_teachersjson():
                                picture=teacher['picture'], price=teacher['price'], goals=','.join(teacher['goals']),
                                free=str(teacher['free'])))
 """
-
-
+"""
+#разовый Импорт
 @app.route('/import/')
 def export():
     import_db_from_teachersjson()
     db.session.commit()
     return 'Сохранено'
+"""
 
 #INDEX
 @app.route('/')
@@ -105,39 +142,26 @@ def goals(goal_id):
         return render_template('goal.html', goal="", teachers=teachers)
 
 
-@app.route('/booking/<id>/<day>/<time>')
+@app.route('/booking/<id>/<day>/<time>', methods=['POST', 'GET'])
 def booking(id, day, time):
+    form = BookinForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            clientName = form.clientName.data
+            clientPhone = form.clientPhone.data
+            clientWeekday = form.clientWeekday.data
+            clientTime = form.clientTime.data
+            clientTeacher = db.session.query(Teacher).get((form.clientTeacher.data))
+            #вот тут не понял как можно было бы использовать populate_obj в связанных
+            booking_table = Booking(clientName=clientName, clientPhone=clientPhone, clientWeekday=clientWeekday,
+                              clientTime=clientTime, clientTeacher=clientTeacher)
+            db.session.add(booking_table)
+            db.session.commit()
+
+            return render_template('booking_done.html', clientName=clientName, clientTime=clientTime, clientPhone=clientPhone,
+                                   clientWeekday=clientWeekday, weekdays=WEEKDAYS_NAMES)
     return render_template('booking.html', teacher=db.session.query(Teacher).get(id),
-                            day=day, time=time, weekdays=WEEKDAYS_NAMES)
-
-
-@app.route('/booking_done/', methods=['POST'])
-def booking_done():
-    clientName = request.form['clientName']
-    clientPhone = request.form['clientPhone']
-    clientWeekday = request.form['clientWeekday']
-    clientTime = request.form['clientTime']
-    clientTeacher = request.form['clientTeacher']
-    new_book = {
-                "clientName": clientName,
-                "clientPhone": clientPhone,
-                "clientWeekday": clientWeekday,
-                "clientTime": clientTime,
-                "clientTeacher": clientTeacher
-                }
-
-    with open('booking.json', 'r') as f:
-        json_bookings = json.load(f)
-    json_bookings.append(new_book)
-    with open('booking.json', 'w') as f:
-        json.dump(json_bookings, f, ensure_ascii=False)
-    return render_template('booking_done.html',
-                           clientName=clientName,
-                           clientTime=clientTime,
-                           clientPhone=clientPhone,
-                           clientWeekday=clientWeekday,
-                           weekdays=weekdays
-                           )
+                            day=day, time=time, weekdays=WEEKDAYS_NAMES, form=form)
 
 
 @app.route('/profile/<id>')
@@ -150,36 +174,24 @@ def profile(id):
                             )
 
 
-@app.route('/search/')
+@app.route('/search/', methods=['POST', 'GET'])
 def search():
-    return render_template('search.html', goals=GOALS_DATA)
+    form = SearchForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            clientGoal = form.clientGoal.data
+            clientTime = form.clientTime.data
+            clientName = form.clientName.data
+            clientPhone = form.clientPhone.data
+            search_table = Search()
+            form.populate_obj(search_table)
+            db.session.add(search_table)
+            db.session.commit()
+            return render_template('search_done.html', goal=GOALS_DATA[clientGoal],
+                                    clientName=clientName, time=clientTime,
+                                    clientPhone=clientPhone)
+    return render_template('search.html', form=form)
 
-
-@app.route('/search_done/', methods=['POST'])
-def search_done():
-    form = request.form.get
-    clientGoal = form('goal')
-    clientTime = form('time')
-    clientName = form('clientName')
-    clientPhone = form('clientPhone')
-    new_search = {
-                  'clientGoal': clientGoal,
-                  'clientTime': clientTime,
-                  'clientName': clientName,
-                  'clientPhone': clientPhone
-                    }
-    with open('search.json', 'r') as f:
-        json_seach = json.load(f)
-    json_seach.append(new_search)
-    with open('search.json', 'w') as f:
-        json_seach = json.dump(json_seach, f, ensure_ascii=False)
-
-    return render_template('search_done.html', goal=goals_data[clientGoal],
-                            clientName=clientName, time=clientTime,
-                            clientPhone=clientPhone
-                            )
-
-print(db.session.query(Teacher).get(0))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
